a3bad44a0275b2a256361846e3821995ed4b7b60
Ruthvik Peddawandla <Ruthvik@Ruthviks-MacBook-Pro.local>
2015-12-07 22:37:49 -0800

    Tests updated

--- karma.conf.js
+++ karma.conf.js
@@ -4,10 +4,10 @@ module.exports = function(config){
     basePath : './',
 
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
+      //'app/bower_components/angular/angular.js',
+      //'app/bower_components/angular-route/angular-route.js',
+      //'app/bower_components/angular-mocks/angular-mocks.js',
+      //'app/components/**/*.js',
       'app/controllers/*.js', 'app/services/*.js', 'server.js', 'app/app.js', 'app/tests/*.js'
     ],
 
